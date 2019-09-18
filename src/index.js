727882823cbc69c7afb2eaeae92861b1f313e358
Firdous <firdous.ali86@gmail.com>
2019-09-18 15:26:01 +0500

    d

--- src/index.js
+++ src/index.js
@@ -18,12 +18,6 @@ const reducers = require('./reducers').default;
 
 applyConfigSettings();
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu'
-});
 class App extends Component {
   state = {
     isLoading: true,
