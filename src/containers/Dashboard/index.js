9f93da5ba7205ef0a19b74e04131ff03a3a03c90
Firdous <firdous.ali86@gmail.com>
2019-09-18 21:18:54 +0500

    working prototype

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -2,37 +2,120 @@
 import { connect } from 'react-redux';
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import { BarIndicator } from 'react-native-indicators';
 import styles from './styles';
+
+import Voice from 'react-native-voice';
 
 import { ButtonView, Text } from '../../components';
 import { Metrics, Colors } from '../../theme';
 
 class Dashboard extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    Voice.onSpeechStart = this.onSpeechStartHandler.bind(this);
+    Voice.onSpeechEnd = this.onSpeechEndHandler.bind(this);
+    Voice.onSpeechResults = this.onSpeechResultsHandler.bind(this);
+    Voice.onSpeechPartialResults = this.onSpeechPartialResultsHandler.bind(
+      this
+    );
+    Voice.onSpeechError = this.onSpeechResultsErrorHandler.bind(this);
+
+    this.state = {
+      isRecording: false,
+      displayString: ''
+    };
+  }
+
+  onSpeechStartHandler = event => {
+    console.log(event);
+
+    this.setState({ isRecording: true });
+  };
+
+  onSpeechEndHandler = event => {
+    console.log(event);
+
+    this.setState({ isRecording: false });
+  };
+
+  onSpeechResultsHandler = event => {
+    console.log(event);
+    const results = event.value.join(' ');
+
+    this.setState({ displayString: results });
+  };
+
+  onSpeechPartialResultsHandler = event => {
+    console.log(event);
+  };
+
+  onSpeechResultsErrorHandler = event => {
+    console.log(event);
+
+    this.setState({ isRecording: false });
+  };
+
+  renderContent = () => {
+    const { displayString } = this.state;
     return (
-      <View style={styles.container}>
-        <View style={{ flex: 1 }}></View>
-        <View
+      <View style={{ flex: 1 }}>
+        <Text color='white'>{displayString}</Text>
+      </View>
+    );
+  };
+
+  renderFooter = () => {
+    return (
+      <View
+        style={{
+          height: Metrics.ratio(70),
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        {this.renderRecordingControl()}
+      </View>
+    );
+  };
+
+  renderRecordingControl = () => {
+    const { isRecording } = this.state;
+
+    if (!isRecording) {
+      return (
+        <ButtonView
           style={{
-            height: Metrics.ratio(70),
+            width: Metrics.ratio(50),
+            height: Metrics.ratio(50),
+            borderRadius: Metrics.ratio(25),
+            borderColor: Colors.white,
+            borderWidth: Metrics.ratio(1),
             justifyContent: 'center',
             alignItems: 'center'
           }}
+          onPress={() => {
+            Voice.start('en-US');
+
+            setTimeout(() => {
+              Voice.stop;
+            }, 5000);
+          }}
         >
-          <ButtonView
-            style={{
-              width: Metrics.ratio(50),
-              height: Metrics.ratio(50),
-              borderRadius: Metrics.ratio(25),
-              borderColor: Colors.themeColors.themeGrey,
-              borderWidth: Metrics.ratio(1),
-              justifyContent: 'center',
-              alignItems: 'center'
-            }}
-          >
-            <Text>R</Text>
-          </ButtonView>
-        </View>
+          <Text color='white'>R</Text>
+        </ButtonView>
+      );
+    }
+
+    return <BarIndicator color='white' count={5} />;
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.renderContent()}
+        {this.renderFooter()}
       </View>
     );
   }
