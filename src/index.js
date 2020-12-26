18fcf26c457da918e903fbbffb5698dae255d9cd
Firdous <firdous.ali86@gmail.com>
2020-12-26 15:14:10 +0500

    fixes

--- src/index.js
+++ src/index.js
@@ -2,17 +2,26 @@
  * @format
  */
 
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import { AppRegistry, Platform, StyleSheet, Text, View } from 'react-native';
-import { name as appName } from '../app.json';
+import React, {Component} from 'react';
+import {Provider} from 'react-redux';
+import {
+  AppRegistry,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  NativeModules,
+} from 'react-native';
+import {name as appName} from '../app.json';
 
 import configureStore from './store';
 import AppNavigator from './navigator';
 import applyConfigSettings from './config';
 import NetworkInfo from './services/NetworkInfo';
-import { networkInfoListener } from './actions/NetworkInfoActions';
-import { DataHelper } from './helpers';
+import {networkInfoListener} from './actions/NetworkInfoActions';
+import {DataHelper} from './helpers';
+
+import Utils from './util';
 
 const reducers = require('./reducers').default;
 
@@ -22,25 +31,27 @@ class App extends Component {
   state = {
     isLoading: true,
     store: configureStore(reducers, () => {
-      this.setState({ isLoading: false }, () => {
+      this.setState({isLoading: false}, () => {
         DataHelper.setStore(this.state.store);
 
         this.loadingCompleted();
       });
-    })
+    }),
   };
 
   componentDidMount() {
+    if (Utils.isPlatformAndroid()) NativeModules.SplashScreen.hide();
+
     NetworkInfo.networkInfoListener(
       this.state.store.dispatch,
-      networkInfoListener
+      networkInfoListener,
     );
   }
 
   componentWillUnmount() {
     NetworkInfo.removeNetworkInfoListener(
       this.state.store.dispatch,
-      networkInfoListener
+      networkInfoListener,
     );
   }
 
@@ -65,8 +76,8 @@ class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
-  }
+    flex: 1,
+  },
 });
 
 AppRegistry.registerComponent(appName, () => App);
